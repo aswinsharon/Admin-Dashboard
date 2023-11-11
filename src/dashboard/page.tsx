939b444283a37ad9714341c57c85710e0e9334cf
Aswin Sharon <aswinsharon@computer.local>
2023-11-11 21:51:08 +0530

    second commit

--- src/dashboard/page.tsx
+++ src/dashboard/page.tsx
@@ -1,12 +1,19 @@
 import React, { ReactNode } from "react";
 import Layout from "./layout";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Products from "./products/page";
+import Users from "./users/page";
+import { childType } from "../../types/Types";
 
-interface DashBoardProps {
-  children?: ReactNode;
-}
-
-const DashBoard: React.FC<DashBoardProps> = () => {
-  return <Layout>Dashboard</Layout>;
+const DashBoard: React.FC<childType> = () => {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/products" element={<Products />}></Route>
+        <Route path="/users" element={<Users />}></Route>
+      </Routes>
+    </Layout>
+  );
 };
 
 export default DashBoard;
