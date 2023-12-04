b995bf8762742db193c873515d2c07019aa9db2d
Aswin Sharon <aswinsharon@computer.local>
2023-12-04 21:31:44 +0530

    added test config

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 import App from "./App";
 
 const root = ReactDOM.createRoot(
